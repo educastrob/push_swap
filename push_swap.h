3c152b912d880eb3e85b9070217455c89389bec7
Eduardo Castro Brito <educastro@MacBook-Air-de-Eduardo.local>
2024-02-22 13:52:58 -0300

    init main

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: educastro <educastro@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 15:43:54 by educastro         #+#    #+#             */
-/*   Updated: 2024/01/22 15:50:03 by educastro        ###   ########.fr       */
+/*   Updated: 2024/01/22 16:45:30 by educastro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,22 @@
 // macros
 # define MAX_INT 2147483647
 # define MIN_INT -2147483648
+
+typedef struct s_node t_node;
+typedef struct s_stack t_stack;
+
+typedef struct	s_node
+{
+	int		value;
+	t_node  *next;
+	t_node	*prev;
+}				t_node;
+
+typedef struct	s_stack
+{
+	int		size;
+	t_node	*top;
+	t_node	*bottom;
+}				t_stack;
 
 #endif
